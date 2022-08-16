80a543689e4d27f6a5a091374d45feeafbefac0d
JJ <jjmoon4682@gmial.com>
2022-08-16 19:21:05 +0900

    초기세팅 수정완료

--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 const { DataSource } = require('typeorm');
 
 const database = new DataSource({
-	type: process.env.TYPEORM_CONNECTION,
+    type: process.env.TYPEORM_CONNECTION,
     host: process.env.TYPEORM_HOST,
     port: process.env.TYPEORM_PORT,
     username: process.env.TYPEORM_USERNAME,
